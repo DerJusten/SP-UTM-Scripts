2549e4c76f24ff0f5144c5dbd11ee742fa32e8a8
DerJusten <DerJusten@gg-ez.win>
2022-05-12 12:57:43 +0200

    update

--- other.sh
+++ other.sh
@@ -2,8 +2,23 @@
 aio_cfg=$dir"/aio.cfg"
 if test -f "$aio_cfg"; then    
     source $aio_cfg
+    ddns=$aio_ddns
 fi
 ## Set DDNS
+if [ "$ddns" = "y" ];then
+    externalInterface="$(spcli interface get |grep external |awk 'BEGIN {FS = "|" }; {print $2}' | xargs)"
+    externalFlagsArr="$(spcli interface get |grep external |awk 'BEGIN {FS = "|" }; {print $4}' | xargs)"
+    externalFlags=""
+    for i in ${externalFlagsArr//,/ }
+    do
+        if [ ! -z $i ];then
+            externalFlags="$externalFlags\"$i\" "
+        fi
+    done
+    ## Flags buggy :C
+    spcli interface set name "$externalInterface" flags [ "$externalFlags"] options [ "dyndns_hostname=maxbenedikt-test.spdns.de" "dyndns_user=maxbenedikt-test.spdns.de" "dyndns_password=jafg-lumb-wyqv" "dyndns_server=update.spdyn.de" "dyndns_mx=" "dyndns_ipv4=1" "dyndns_ipv6=0" "dyndns_webresolver=http://checkip4.spdyn.de/" ]
+
+fi
 
 
 
